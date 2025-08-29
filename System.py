fdb338b35c20079c6d004f18627362c89b7ce71b
rotem-bo-MA <rotem.bol@moonactive.com>
2025-08-29 17:28:50 +0300

    fix the System imports

--- System.py
+++ System.py
@@ -1,4 +1,4 @@
-from data_structures_starte import messages_data_structure, users_data_structure, friends_data_structure, likes_data_structure
+from data_structures_starter import messages_data_structure, users_data_structure, friends_data_structure, likes_data_structure
 
 class System:
     def __init__(self):
