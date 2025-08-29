a22b656cfa3bb392e97fb470c7d47072a02a4dcc
rotem-bo-MA <rotem.bol@moonactive.com>
2025-08-29 16:17:10 +0300

    insert the def into the class

--- AVLTree_class.py
+++ AVLTree_class.py
@@ -1,73 +1,88 @@
-from dataclasses import dataclass
 from typing import Optional, Iterable
 from Users_class import User
 
-@dataclass
 class _Node:
-    key: int
-    val: User
-    h: int = 1
-    left: Optional["_Node"] = None
-    right: Optional["_Node"] = None
+    def __init__(self, key: int, val: User, h: int = 1, left: Optional["_Node"] = None, right: Optional["_Node"] = None):
+        self.key = key
+        self.val = val
+        self.h = h
+        self.left = left
+        self.right = right
 
-def _height(t: Optional[_Node]) -> int:
-    return t.h if t else 0
+    @staticmethod
+    def _height(t: Optional["_Node"]) -> int:
+        return t.h if t else 0
 
-def _fix(t: _Node) -> _Node:
-    t.h = max(_height(t.left), _height(t.right)) + 1
-    return t
+    def _fix(self) -> "_Node":
+        self.h = max(_Node._height(self.left), _Node._height(self.right)) + 1
+        return self
 
-def _bf(t: Optional[_Node]) -> int:
-    return (_height(t.left) - _height(t.right)) if t else 0
+    @staticmethod
+    def _bf(t: Optional["_Node"]) -> int:
+        return (_Node._height(t.left) - _Node._height(t.right)) if t else 0
 
-def _rotR(y: _Node) -> _Node:
-    x = y.left; T2 = x.right
-    x.right = y; y.left = T2
-    _fix(y); _fix(x); return x
+    def _rotR(self) -> "_Node":
+        x = self.left
+        T2 = x.right
+        x.right = self
+        self.left = T2
+        self._fix()
+        x._fix()
+        return x
 
-def _rotL(x: _Node) -> _Node:
-    y = x.right; T2 = y.left
-    y.left = x; x.right = T2
-    _fix(x); _fix(y); return y
+    def _rotL(self) -> "_Node":
+        y = self.right
+        T2 = y.left
+        y.left = self
+        self.right = T2
+        self._fix()
+        y._fix()
+        return y
 
-def _rebalance(t: _Node) -> _Node:
-    _fix(t)
-    b = _bf(t)
-    if b > 1:
-        if _bf(t.left) < 0:
-            t.left = _rotL(t.left)
-        return _rotR(t)
-    if b < -1:
-        if _bf(t.right) > 0:
-            t.right = _rotR(t.right)
-        return _rotL(t)
-    return t
+    def _rebalance(self) -> "_Node":
+        self._fix()
+        b = _Node._bf(self)
+        if b > 1:
+            if _Node._bf(self.left) < 0:
+                self.left = self.left._rotL()
+            return self._rotR()
+        if b < -1:
+            if _Node._bf(self.right) > 0:
+                self.right = self.right._rotR()
+            return self._rotL()
+        return self
 
-def _insert(t: Optional[_Node], key: int, val: User) -> _Node:
-    if not t:
-        return _Node(key, val)
-    if key < t.key:
-        t.left = _insert(t.left, key, val)
-    elif key > t.key:
-        t.right = _insert(t.right, key, val)
-    else:
-        # key קיים: עדכון הערך
-        t.val = val
-        return t
-    return _rebalance(t)
+    @staticmethod
+    def _insert(t: Optional["_Node"], key: int, val: User) -> "_Node":
+        if not t:
+            return _Node(key, val)
+        if key < t.key:
+            t.left = _Node._insert(t.left, key, val)
+        elif key > t.key:
+            t.right = _Node._insert(t.right, key, val)
+        else:
+            # key קיים: עדכון הערך
+            t.val = val
+            return t
+        return t._rebalance()
 
-def _get(t: Optional[_Node], key: int) -> Optional[User]:
-    while t:
-        if key < t.key: t = t.left
-        elif key > t.key: t = t.right
-        else: return t.val
-    return None
+    @staticmethod
+    def _get(t: Optional["_Node"], key: int) -> Optional[User]:
+        while t:
+            if key < t.key: 
+                t = t.left
+            elif key > t.key: 
+                t = t.right
+            else: 
+                return t.val
+        return None
 
-def _inorder(t: Optional[_Node]) -> Iterable[User]:
-    if t:
-        yield from _inorder(t.left)
-        yield t.val
-        yield from _inorder(t.right)
+    @staticmethod
+    def _inorder(t: Optional["_Node"]) -> Iterable[User]:
+        if t:
+            yield from _Node._inorder(t.left)
+            yield t.val
+            yield from _Node._inorder(t.right)
 
 class AVLTree:
     def __init__(self):
@@ -78,14 +93,14 @@ class AVLTree:
 
     def insert(self, key: int, val: User) -> None:
         existed = self.search(key) is not None
-        self._root = _insert(self._root, key, val)
+        self._root = _Node._insert(self._root, key, val)
         if not existed: self._n += 1
 
     def search(self, key: int) -> Optional[User]:
-        return _get(self._root, key)
+        return _Node._get(self._root, key)
 
     def inorder(self) -> Iterable[User]:
-        return _inorder(self._root)
+        return _Node._inorder(self._root)
 
 
 def load_users_into_tree(tree: AVLTree, users_data: list[tuple]) -> None:
